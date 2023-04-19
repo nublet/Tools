f00ac985e2c62f84ee1bc605bbe4d5a92937eb08
Bjorn Falt <n00bl0rd.mail@gmail.com>
2023-04-19 18:00:02 +0100

    SIA Updates

--- _Main.vb
+++ _Main.vb
@@ -4,8 +4,7 @@
 
         Public Sub Main(args As String())
             Try
-                If CommonRoutines.SingleInstanceApplication.AlreadyExists(WindowCaption) Then
-                    CommonRoutines.SingleInstanceApplication.NotifyExistingInstance(String.Join(CommonRoutines.ParameterSplit, args), WindowCaption)
+                If CommonRoutines.SingleInstanceApplication.NotifyExistingInstance(String.Join(CommonRoutines.ParameterSplit, args), WindowCaption) Then
                     Application.Exit()
                     Return
                 End If
@@ -20,14 +19,19 @@
                 Application.SetCompatibleTextRenderingDefault(False)
                 Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException)
 
-                CommonRoutines.Initialise("D:\Projects\_Errors\{0} - {1}_{2}.txt".FormatWith(My.Application.Info.ProductName, CommonRoutines.GetComputerName(), CommonRoutines.GetUsername()), False, "", False)
+                CommonRoutines.Initialise("\\192.168.50.35\Projects\_Errors\{0} - {1}_{2}.txt".FormatWith(My.Application.Info.ProductName, CommonRoutines.GetComputerName(), CommonRoutines.GetUsername()), False, "", False)
 
                 CommonRoutines.Settings.Icon = My.Resources.App
 
-                CommonRoutines.SingleInstanceApplication.Initialize(WindowCaption, New CommonRoutines.SingleInstanceApplication.NewInstanceMessageHandler(AddressOf SIA_NewInstanceMessage))
-
                 CommonRoutines.Settings.DebugLogging = False
                 CommonRoutines.Settings.DebugLoggingDBAccess = False
+
+                'CommonRoutines.Errors.Handlers.Clear()
+                'CommonRoutines.Errors.Handlers.Add(New CommonRoutines.Errors.LogErrorDetailHandler(AddressOf LogError))
+
+                CommonRoutines.Performance.Handlers.Clear()
+
+                CommonRoutines.SingleInstanceApplication.Initialize(WindowCaption, New CommonRoutines.SingleInstanceApplication.NewInstanceMessageHandler(AddressOf SIA_NewInstanceMessage))
 
                 Application.Run(New UserForms.Main())
             Catch ex As Exception
